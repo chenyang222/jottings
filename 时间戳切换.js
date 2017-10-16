8a5d0d6cef021165115d9f102144167f4fb8e6f9
ranchy <chenyang222@users.noreply.github.com>
2017-10-16 16:35:33 +0800

    2017/10/16

---
+++ 时间戳切换.js
@@ -0,0 +1,49 @@
+//时间戳转 2017-10-16 16:33:00
+function formatDateTime(inputTime) {    
+    var date = new Date(inputTime);  
+    var y = date.getFullYear();    
+    var m = date.getMonth() + 1;    
+    m = m < 10 ? ('0' + m) : m;    
+    var d = date.getDate();    
+    d = d < 10 ? ('0' + d) : d;    
+    var h = date.getHours();  
+    h = h < 10 ? ('0' + h) : h;  
+    var minute = date.getMinutes();  
+    var second = date.getSeconds();  
+    minute = minute < 10 ? ('0' + minute) : minute;    
+    second = second < 10 ? ('0' + second) : second;   
+    return y + '-' + m + '-' + d+' '+h+':'+minute+':'+second;    
+};  
+//日期转时间戳
+function format(time, format) {
+    var t = new Date(time);
+    var tf = function(i) {
+        return(i < 10 ? '0' : '') + i
+    };
+    if(time == null) {
+        return '';
+    } else {
+        return format.replace(/yyyy|MM|dd|HH|mm|ss/g, function(a) {
+            switch(a) {
+                case 'yyyy':
+                    return tf(t.getFullYear());
+                    break;
+                case 'MM':
+                    return tf(t.getMonth() + 1);
+                    break;
+                case 'mm':
+                    return tf(t.getMinutes());
+                    break;
+                case 'dd':
+                    return tf(t.getDate());
+                    break;
+                case 'HH':
+                    return tf(t.getHours());
+                    break;
+                case 'ss':
+                    return tf(t.getSeconds());
+                    break;
+            }
+        })
+    }
+}
